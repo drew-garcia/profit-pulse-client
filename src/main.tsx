f6e14f5b8debf02ce99860720c2ff3cf20dfd9c9
Andrew Garcia <116861932+drew-garcia@users.noreply.github.com>
2024-07-21 18:06:06 -0400

    material ui setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 import './assets/styles/index.css'
 import {ApolloProvider} from '@apollo/client'
 import client from './apolloClient.ts'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
